680e035dde838b6cfb8ba675678dfe5c304be650
murongxixi <murongxixi@gmail.com>
2021-11-04 08:43:31 +0800

    update

--- upload.sh
+++ upload.sh
@@ -2,5 +2,19 @@ cp ~/.mume/parser.js ./common/js/
 cp ~/.mume/mathjax_config.js ./common/js/
 git add *
 git commit -m $1
-git push github master
-# git push gitee master
+
+case $2 in
+"ee")
+    git push gitee master
+    ;;
+"hub")
+    git push github master
+    ;;
+"both")
+    git push gitee master
+    git push github master
+    ;;
+*)
+    echo "error: 2nd par must be [ee|hub|both]!"
+    ;;
+esac
